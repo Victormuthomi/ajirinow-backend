3d044cd25a51c9f9411079e0ee95135f0624b2c7
Victormuthomi <victormuthomi100@gmail.com>
2025-07-07 18:02:43 +0300

    Remove hardcoded secrets and use .env config

--- ajirinow/settings.py
+++ ajirinow/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 import os
 
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7do6!klifi+#u0*d!zzbyq#ym@)wua&8v%@6*s&2r0!$)zwmpt'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,13 +86,14 @@ WSGI_APPLICATION = 'ajirinow.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ajirinow_db',
-        'USER': 'victor',
-        'PASSWORD': '10183510',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
+
 
 
 
