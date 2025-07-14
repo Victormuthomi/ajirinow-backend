06d1f96d05c2454f44a8eb478b654da5f1d22993
Victormuthomi <victormuthomi100@gmail.com>
2025-07-15 01:13:45 +0300

    modify cors settings.

--- ajirinow/settings.py
+++ ajirinow/settings.py
@@ -183,9 +183,7 @@ CALLBACK_URL = os.environ.get("CALLBACK_URL", "http://127.0.0.1:8000/api/mpesa/c
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
-    "https://ajirinow.vercel.app",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "*",
 ]
 
 CORS_ALLOW_HEADERS = [
